fc5463c919c8bcde05058799b17b50209286653a
jsmo <42164262+Z2R0Sum@users.noreply.github.com>
2019-06-25 18:45:24 +0900

    백준 11399 ATM Greedy 진행 중.

---
+++ src/Greedy/ATM11399_Boj.java
@@ -0,0 +1,15 @@
+package Greedy;
+
+import java.util.Scanner;
+
+public class ATM11399_Boj {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int arr[] = new int[N];
+		for(int i=0; i<N; i++) {
+			arr[i] = sc.nextInt();
+		}
+	}
+}
