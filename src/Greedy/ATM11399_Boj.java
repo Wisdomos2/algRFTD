6974eb4d4a2d158ee9e6d4d9b1302b05ce40e720
jsmo <42164262+Z2R0Sum@users.noreply.github.com>
2019-06-27 11:30:12 +0900

    동기화

--- src/Greedy/ATM11399_Boj.java
+++ src/Greedy/ATM11399_Boj.java
@@ -1,15 +1,28 @@
 package Greedy;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ATM11399_Boj {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		int result = 0;
+		
 		int N = sc.nextInt();
 		int arr[] = new int[N];
 		for(int i=0; i<N; i++) {
 			arr[i] = sc.nextInt();
 		}
+		
+		Arrays.sort(arr);
+		result = arr[0];
+	
+		for(int i=1; i<N; i++) {
+			arr[i] = arr[i-1] + arr[i];
+			result += arr[i];
+		}
+		
+		System.out.println(result);
 	}
 }
