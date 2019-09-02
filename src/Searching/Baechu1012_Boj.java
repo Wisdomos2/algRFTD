d2211cd3c4523b05a9a859d66394d7fb4dacb919
MOJUNSEO <mojunseo49@naver.com>
2019-09-02 11:13:20 +0900

    진짜 개ㅓ이없는걸로 이거 수정한다고 몇번을 삽질한건지 모르겠네 그냥 얌전히 col row 로 쳐맞을것이지 중간에 말장난 장난하냐 ㄹㅇ루다가 빠따각

--- src/Searching/Baechu1012_Boj.java
+++ src/Searching/Baechu1012_Boj.java
@@ -5,12 +5,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
 /*
  * 이거풀고 백트래킹 2개정도 풀 예정.
  */
 public class Baechu1012_Boj {
 	static BufferedReader bf = null;
+	static StringTokenizer st = null;
 	static int testcase = 0;
 	static int row = 0;
 	static int col = 0;
@@ -20,73 +22,65 @@ public class Baechu1012_Boj {
 	static Queue<location> q = null;
 	static int dc[] = {0,0,1,-1};
 	static int dr[] = {1,-1,0,0};
+	static int printarr[] = null;
 	public static void main(String[] args) throws IOException{
 		bf = new BufferedReader(new InputStreamReader(System.in));
 		
 		testcase = Integer.parseInt(bf.readLine());
+		printarr = new int[testcase];
 		
 		for(int i=0;i<testcase;i++) {
-			String temp[] = bf.readLine().split(" ");
-			row = Integer.parseInt(temp[0]);
-			col = Integer.parseInt(temp[1]);
-			chu = Integer.parseInt(temp[2]);
+			st = new StringTokenizer(bf.readLine()," ");
+			row = Integer.parseInt(st.nextToken());
+			col = Integer.parseInt(st.nextToken());
 			
-			if(row < -1 || row > 50 || col < -1 || col > 50 || chu < -1 || chu > 2500) {
-				return;
+			map = new int[col][row];
+			visited = new boolean[col][row];
+			
+			chu = Integer.parseInt(st.nextToken());
+			
+			for(int j=0;j<chu;j++) {
+				st = new StringTokenizer(bf.readLine(), " ");
+				int tempRow = Integer.parseInt(st.nextToken());
+				int tempCol = Integer.parseInt(st.nextToken());
+				if(tempCol > -1 && tempCol < col && tempRow > -1 && tempRow < row) {
+					map[tempCol][tempRow] = 1;
+				}
 			}
-			else {
-				map = new int[col][row];
-				visited = new boolean[col][row];
-				q = new LinkedList<location>();
-				
-				for(int j=0;j<chu;j++) {
-					String chus[] = bf.readLine().split(" ");
-					int inputcol = Integer.parseInt(chus[0]);
-					int inputrow = Integer.parseInt(chus[1]);
-					if(inputcol < 0 || inputcol > (col-1) || inputrow < 0 || inputrow > (row-1)) {
-						continue;
-					}
-					else {
-						map[inputcol][inputrow] = 1;
+			
+			int insect = 0;
+			for(int a=0; a<col; a++) {
+				for(int b=0; b<row; b++) {
+					if(map[a][b] == 1 && visited[a][b] == false) {
+						Search(a,b);
+						insect++;
 					}
 				}
-				
-				for(int t = 0; t < col ; t++) {
-					for(int y = 0; y<row; y++) {
-						System.out.print(map[t][y]);
-					}
-					System.out.println();
-				}
-				/*
-				 * start to imply logic
-				 */
-				int insect  = 0;
-				for(int a=0; a<col;a++) {
-					for(int b=0;b<row;b++) {
-						if(map[a][b] == 1 && visited[a][b] == false) {
-							q.offer(new location(a,b));
-							visited[a][b] = true;
-							System.out.println(a + " / " + b);
-							insect++;
-							while(!q.isEmpty()) {
-								int nowc = q.peek().col;
-								int nowr = q.poll().row;
-								
-								for(int k=0;k<4;k++) {
-									int nextc = nowc + dc[i];
-									int nextr = nowr + dr[i];
-									if(nextc > -1 && nextc < col && nextr > -1 && nextr < row) {
-										if(map[nextc][nextr] == 1 && visited[nextc][nextr] == false) {
-											visited[nextc][nextr] = true;
-											q.offer(new location(nextc,nextr));
-										}
-									}
-								}
-							}
-						}
+			}
+			printarr[i] = insect;
+		}
+		for(int i : printarr) {
+			System.out.println(i);
+		}
+	}
+	
+	static private void Search(int a, int b) {
+		q = new LinkedList<location>();
+		q.offer(new location(a,b));
+		visited[a][b] = true;
+		while(!q.isEmpty()) {
+			int nowc = q.peek().col;
+			int nowr = q.poll().row;
+			
+			for(int k=0;k<4;k++) {
+				int nextc = nowc + dc[k];
+				int nextr = nowr + dr[k];
+				if(nextc > -1 && nextc < col && nextr > -1 && nextr < row) {
+					if(map[nextc][nextr] == 1 && visited[nextc][nextr] == false) {
+						q.offer(new location(nextc,nextr));
+						visited[nextc][nextr] = true;
 					}
 				}
-				System.out.println(insect);
 			}
 		}
 	}
