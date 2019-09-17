dd1e572c322a977642b3acb4768a3fafb89995a8
MOJUNSEO <mojunseo49@naver.com>
2019-09-17 22:06:57 +0900

    제말 문제 풀 때 변수 값 잘 들어갔는지, 잘 봐..뻘짓하지말고

--- src/Implement/MoneyHunter15953_Boj.java
+++ src/Implement/MoneyHunter15953_Boj.java
@@ -11,17 +11,14 @@ public class MoneyHunter15953_Boj {
         /*
             init
          */
-        chell1 = new int[][]{{1,5000000,1,1}, {2,3000000,2,0}, {3,2000000,3,0}, {4,500000,4,0}, {1,3000000,5,0}, {1,1000000,6,0}};
-        chell2 = new int[][]{{1,5120000,1,1}, {1,2560000,2,0}, {1,1280000,4,0}, {1,640000,8,0}, {1,320000,16,0}};
+        chell1 = new int[][]{{1,5000000,1,1}, {2,3000000,2,0}, {3,2000000,3,0}, {4,500000,4,0}, {5,300000,5,0}, {6,100000,6,0}};
+        chell2 = new int[][]{{1,5120000,1,1}, {2,2560000,2,0}, {3,1280000,4,0}, {4,640000,8,0}, {5,320000,16,0}};
 
         for(int i=1;i<6;i++) {
             chell1[i][3] = chell1[i-1][3] + chell1[i][2];
-            System.out.println(chell1[i][3]);
         }
-        System.out.println();
         for(int i=1;i<5;i++) {
             chell2[i][3] = chell2[i-1][3] + chell2[i][2];
-            System.out.println(chell2[i][3]);
         }
 
 
@@ -39,17 +36,38 @@ public class MoneyHunter15953_Boj {
     static public int solution(int input1, int input2) {
         int result = 0;
 
+
         for(int i=1;i<6;i++) {
-            if(input1 >= chell1[i][3] && input1 < chell1[i-1][3]) {
-                result += chell1[i][1];
+            if(input1 == 1) {
+                result += chell1[0][1];
                 break;
+            }
+            else if(input1 > chell1[5][3]) {
+                result += 0;
+                break;
+            }
+            else {
+                if(input1 <=chell1[i][3] && input1 > chell1[i-1][3]) {
+                    result += chell1[i][1];
+                    break;
+                }
             }
         }
 
         for(int i=1;i<5;i++) {
-            if(input2 >= chell2[i][3] && input1 < chell2[i-1][3] ) {
-                result += chell2[i][1];
+            if(input2 == 1) {
+                result += chell2[0][1];
                 break;
+            }
+            else if(input2 > chell2[4][3]) {
+                result += 0;
+                break;
+            }
+            else {
+                if(input2 <= chell2[i][3] && input2 > chell2[i-1][3] ) {
+                    result += chell2[i][1];
+                    break;
+                }
             }
         }
 
---
+++ src/Implement/ShortCoding15956_Boj.java
@@ -0,0 +1,11 @@
+package Implement;
+/*
+    https://www.acmicpc.net/problem/15956
+ */
+
+
+public class ShortCoding15956_Boj {
+    public static void main(String[] args) {
+
+    }
+}
