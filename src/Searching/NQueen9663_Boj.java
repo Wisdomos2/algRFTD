6ef6d3d3ce2d998c165873f8d6e61998a36d8779
wisdomose <mojunseo49@naver.com>
2019-11-09 22:20:43 +0900

    백트래킹 알고리즘 풀이

---
+++ src/Searching/NQueen9663_Boj.java
@@ -0,0 +1,27 @@
+package Searching;
+
+import java.util.*;
+
+public class NQueen9663_Boj {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int arr[][] = new int[N][N];
+
+        for(int i=0;i<N;i++) {
+            for(int j=0;j<N;j++) {
+                Queen();
+            }
+        }
+
+
+    }
+
+    public static void Queen() {
+
+    }
+
+}
