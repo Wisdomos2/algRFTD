5b8ef9d98a9899955e977b384ad978e1c186d252
wisdomose <mojunseo49@naver.com>
2019-11-12 06:56:02 +0900

    124나라 풀이.

--- src/study/NARA124_Prg.java
+++ src/study/NARA124_Prg.java
@@ -13,52 +13,35 @@ public class NARA124_Prg {
        // System.out.println(solution(n1));
        // System.out.println(solution(n2));
        // System.out.println(solution(n3));
-        System.out.println(solution(53535));
+        System.out.println(solution(5000000));
 
         return;
     }
 
+    /*
+     10진법 표를 보고 나누기 3을 해서 몫과 나머지를 보고 판단하는 문제 .
+     나머지가 1이면 뒤에 왼쪽으로 1을 붙임, 2이면 2를 붙임 나머지가 0이면 4를 붙임, 대신 몫에서 1을 빼줌.
+     첨에 브루트포스로 다 만들었다가 ㅈ망함.
+     */
     public static String solution(int n) {
-        /*
-        * 큐사용하지 않고 해보기. 5000000이라는 조건 다 넘겨야함.
-        * 3의 1승 , 3의 2승 식으로 반복횟수가 증가됨.
-        * */
-        String answer = null;
+        String answer = "";
+        String info[] = {"4","1","2"};
 
-        String arr[] = new String[n];
-        Queue<String> q = new LinkedList<String>();
-        arr[0] = "1";
-        arr[1] = "2";
-        arr[2] = "4";
+        int fv = n;
+        int rv = 0;
 
-        if(n < 4) {
-            answer = arr[n-1];
-            return answer;
-        }
+        while(fv != 0) {
+            fv = n/3;
+            rv = n%3;
 
-        q.add(arr[0]);
-        q.add(arr[1]);
-        q.add(arr[2]);
-
-        String info[] = {"1","2","4"};
-        for(int i=3;i<n;) {
-            int qsize = q.size();
-            for(int k=0;k<qsize;k++) {
-                String getNum = q.poll();
-                for(int j=0;j<3;j++) {
-                    arr[i+j] = getNum + ""+ info[j];
-                    if(i+j == n-1) {
-                        answer = arr[i+j];
-                        return answer;
-                    }
-                    q.add(arr[i+j]);
-                }
-                i = i+3;
+            if(rv == 0) {
+                fv -= 1;
             }
+            answer = info[rv] + answer;
+            n = fv;
         }
 
         return answer;
     }
-
 
 }
