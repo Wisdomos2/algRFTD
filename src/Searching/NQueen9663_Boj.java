6b0baf05a4deef76396c1668586569ea86b5e6c2
wisdomose <mojunseo49@naver.com>
2019-11-12 04:20:46 +0900

    이그노어 업데이트

--- src/Searching/NQueen9663_Boj.java
+++ src/Searching/NQueen9663_Boj.java
@@ -3,25 +3,57 @@ package Searching;
 import java.util.*;
 
 public class NQueen9663_Boj {
+    static int col[] = null;
+    static int N = 9;
+    static int result = 0;
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
+        N = sc.nextInt();
 
-        int arr[][] = new int[N][N];
-
-        for(int i=0;i<N;i++) {
-            for(int j=0;j<N;j++) {
-                Queen();
-            }
+        for(int i=1;i<=N;i++) {
+            col = new int[N+1];
+            col[1] = i;
+            dfs(1);
         }
 
 
+        System.out.println(result);
     }
 
-    public static void Queen() {
-
+    public static void dfs(int row) {
+        if(row == N) {
+            ++result;
+        }
+        else {
+            for(int i=1;i<=N;i++) {
+                col[row+1] = i;
+                if(check(row+1)) {
+                    dfs(row+1);
+                }
+                else {
+                    col[row+1] = 0;
+                }
+            }
+        }
+        //back tracking
+        col[row] = 0;
     }
+    // 행의 차 절대값 == 열의 차 절대값 이 성립하면 두 좌표는 동일 대각선.
+    public static boolean check(int row) {
+
+        for(int i=1;i<row;i++) {
+            if(col[i] == col[row]) {
+                return false;
+            }
+            if(Math.abs(col[i] - col[row]) == Math.abs(i-row)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
 
 }
